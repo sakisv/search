7346afda4e725a8ba61d83e45c8e83f0a618c7bf
Sakis V <sakisv@sharehood.co.uk>
2013-11-26 18:41:33 +0000

    put everything under search module

--- search/index/__init__.py
+++ search/index/__init__.py
@@ -1,3 +1,4 @@
+from .stopwords import STOPWORDS
 from nltk.stem import SnowballStemmer
 from nltk.stem.isri import ISRIStemmer
 from bs4 import BeautifulSoup
@@ -18,11 +19,11 @@ class Indexer(object):
             'body': 1,
             'title': 1.5
         }
-        
+
         self.id = 'id'
 
-    def create_reverse_index(self, documents, lang_code='en', fields=None, prefix='', 
-            id_field=''):
+    def create_reverse_index(self, documents, lang_code='en', fields=None,
+            prefix='', id_field=''):
         '''
         Creates the reverse for the list of document passed as argument
         arguments:
@@ -38,12 +39,16 @@ class Indexer(object):
         if fields:
             self.fields = fields
 
+        # make sure even one document is parsed as list
+        if type(documents) != list:
+            documents = [documents]
         documents = self._clean_and_fix(documents)
-        
+
+        self.collection = {}
         for document in documents:
             for field, value in self.fields.iteritems(): 
                 if hasattr(document, field):
-
+                    self._create_word_collection(getattr(document, field))
 
     def _get_stemmer(self, lang_code):
         '''
@@ -83,3 +88,54 @@ class Indexer(object):
                     # update object with the clean text
                     setattr(document, field, text)
         return documents
+
+    def _create_word_collection(self, document):
+        '''
+        Parse the document and create a collection of words and their scores
+        '''
+        words = document.split(' ')
+
+        # if a word is not in STOPWORDS and is not too small, keep it's stem
+        words = [self.stemmer.stem(x) for x in words if x > 2 and x not in STOPWORDS]
+        count = len(words)
+
+        # count the presence of each word
+        collection = {}
+        for w in words:
+            collection[w] = (collection.get(w, 0)) + 1
+
+        # frequency of each word
+        for w in collection.keys():
+            collection[w] /= float(count) * 10
+
+        return collection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ search/search/__init__.py
@@ -0,0 +1,2 @@
+class Search(object):
+    pass
