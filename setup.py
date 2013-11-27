b92a961f84abb10aed894fe64e37836090bc4293
Sakis V <sakisv@sharehood.co.uk>
2013-11-27 11:52:02 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='search',
+    version='0.1',
+    author='sakisv',
+    url='https://github.com/sakisv/search',
+    install_requires=[
+        'nltk==2.0.4',
+        'pyyaml==3.10',
+        'beautifulsoup4==4.1.3'
+    ],
+)
