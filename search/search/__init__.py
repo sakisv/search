57b060e9fe68d3bdc35a5177c6c3978f308098ec
Sakis V <sakisv@sharehood.co.uk>
2013-11-29 02:03:18 +0000

    Start wiring in search

--- search/search/__init__.py
+++ search/search/__init__.py
@@ -1,2 +1,58 @@
+from ..util import clean_and_fix
+
+CONTENT_MAP = {
+    'p': StaticPage,
+    's': Page,
+    'a': Page,
+}
+
 class Search(object):
-    pass
+    def __init__(self, redis):
+        self.redis = redis
+
+    def search(self, query):
+        context = {'q': query}
+
+        # collect term indexed data from redis
+        data = {}
+        words = self._tokeniser(query)
+        for word in words:
+            key = 'index:*:%s' % word
+            data[key] = self.redis.zrange(key, 0, -1, withscores=True)
+
+        # process data to produce set of result documents
+        union = {}
+        for docs in data.values():
+            for doc_id, score in docs:
+                if doc_id not in union:  # initialising
+                    union[doc_id] = {}
+                    union[doc_id]['score'] = score
+                    union[doc_id]['count'] = 1
+                else:  # aggregating
+                    union[doc_id]['score'] += score
+                    union[doc_id]['count'] += 1
+
+        # rank result documents
+        ranked = sorted(union,
+            key=lambda x: union[x]['score'] * union[x]['count'], reverse=True)
+
+        # retrieve results from redis
+        results = []
+        for page_key in ranked:
+            page_id, lang = page_key.split('_')
+            page = CONTENT_MAP[page_id[0]]('', page_id, lang)
+            try:
+                page.retrieve()
+            except BadUrlForContent:
+                pass
+            except NoSuchContent:
+                continue
+            results.append(page.content)
+        context['results'] = results
+        return render(request, 'search.html', context)
+
+
+    def _tokeniser(self, query):
+        # first fix and clean it
+        query = clean_and_fix(query)
+        return query.split(' ')
